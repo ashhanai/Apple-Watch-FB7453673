7e66dc5b1c41cbcfe352134bbb02c776fa2eb9e7
Naim Ashhab <naim.ashhab@cleverlance.com>
2020-01-13 13:08:36 +0100

    Initial Commit

---
+++ Apple-Watch-FB7453673/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple-Watch-FB7453673
+//
+//  Created by Naim Ashhab on 13/01/2020.
+//  Copyright © 2020 Naim Ashhab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
